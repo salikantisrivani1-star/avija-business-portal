4a77a3a669492f91846f718b3834af1e65803a5d
salikantisrivani1-star <salikantisrivani1@gmail.com>
2026-07-17 12:11:36 +0530

    file name case for render

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import nodemailer from "nodemailer";
-import Contact from "../models/Contact.js";
+import Contact from "../models/contact.js";
 
 const router = express.Router();
 
